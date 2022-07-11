10051b433a8adb7a352f90dc68f5999e80ce9f54
Ceresveres <ceresveres@gmail.com>
2022-07-11 14:23:23 -0700

    Create loops.cpp

---
+++ cpp/loops.cpp
@@ -0,0 +1,31 @@
+While loop
+
+bool a = true;
+while (a) {
+  if (a == true) {
+    a = false 
+  }
+}
+
+int count = 10;
+while (count > 0) {
+   //do a thing
+  count--;
+}
+
+
+
+Do While loop
+
+int count = 0;
+Do
+  count++;
+while (count < 10);
+
+
+
+For loop
+
+for (int i = 0; i < 10; i++) {
+  //do a thing;
+}
