13eb91c9b3ec4ba3e646c3cfe7bffbeb2f0bbf10
Ceresveres <ceresveres@gmail.com>
2022-07-11 14:07:19 -0700

    Create addingtwonumbers.cpp

---
+++ cpp/addingtwonumbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int addingTwoNumbers() {
+	int a, b;
+	cout << "Input two numbers: " << endl;
+	cin >> a >> b;
+	cout << a + b << endl;
+	return a + b;
+}
